57b7283296d4ea831e6a8eeebab5322caf0a3393
zhupengliang <zpl_dongxie@sina.com>
2019-12-19 13:39:05 +0800

    通过antd pro创建基本结构

---
+++ src/pages/JcfxList/model.js
@@ -0,0 +1,60 @@
+/*
+ * @description:
+ * @author: zpl
+ * @Date: 2019-12-10 17:17:45
+ * @LastEditTime: 2019-12-16 15:27:59
+ * @LastEditors: zpl
+ */
+import { addRule, queryRule, removeRule, updateRule } from './service';
+
+const Model = {
+  namespace: 'jcfxList',
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+  },
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      const response = yield call(queryRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+    },
+
+    *add({ payload, callback }, { call, put }) {
+      const response = yield call(addRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+
+    *remove({ payload, callback }, { call, put }) {
+      const response = yield call(removeRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+
+    *update({ payload, callback }, { call, put }) {
+      const response = yield call(updateRule, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+      if (callback) callback();
+    },
+  },
+  reducers: {
+    save(state, action) {
+      return { ...state, data: action.payload };
+    },
+  },
+};
+export default Model;
